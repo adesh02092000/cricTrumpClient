73bf3673e387887fe2b9fffc85c8b9535600195a
Adesh Gupta <adesh02092000@gmail.com>
2023-06-14 17:12:56 +0530

    updated prodution things

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  // baseURL: 'http://localhost:5000/',
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://odd-gold-newt-slip.cyclic.app/',
 })
 
 export default async function getPlayers() {
